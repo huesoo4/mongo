593c888a46977c47e218cb35831e7570e87e8920
huesoo4 <josemanuellopezhueso@gmail.com>
2026-05-07 20:20:27 +0200

    Main with menu and all options of program

---
+++ main.py
@@ -0,0 +1,55 @@
+from mongo_def import *
+
+
+def menu():
+
+    opcion = ""
+
+    while opcion != "9":
+
+        print("\n========== MENÚ MONGODB ==========")
+        print("1. Insertar estudiante")
+        print("2. Eliminar estudiante")
+        print("3. Modificar estudiante")
+        print("4. Consulta simple")
+        print("5. Consulta con arrays")
+        print("6. Consulta con documentos embebidos")
+        print("7. Consulta con array de documentos")
+        print("8. Consulta de agrupación")
+        print("9. Salir")
+
+        opcion = input("Elige una opción: ")
+
+        if opcion == "1":
+            insertar_documento()
+
+        elif opcion == "2":
+            eliminar_documento()
+
+        elif opcion == "3":
+            modificar_documento()
+
+        elif opcion == "4":
+            consulta_simple()
+
+        elif opcion == "5":
+            consulta_array()
+
+        elif opcion == "6":
+            consulta_documento_embebido()
+
+        elif opcion == "7":
+            consulta_scores()
+
+        elif opcion == "8":
+            consulta_agrupacion()
+
+        elif opcion == "9":
+            print("Saliendo del programa...")
+
+        else:
+            print("Opción no válida")
+
+
+if __name__ == "__main__":
+    menu()
